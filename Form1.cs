19c69f2de4c80d3ccad920f9b4dc3becec976098
maldron0309 <bagjinhyeong640@gmail.com>
2024-01-14 20:26:21 +0900

    주석

--- Form1.cs
+++ Form1.cs
@@ -17,52 +17,63 @@ namespace Compression_Program
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Compress 버튼 클릭 시 실행
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Compress_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
 
+            // 사용자가 파일을 선택
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 int L1, L2;
-                var bytes = System.IO.File.ReadAllBytes(ofd.FileName);
+                var bytes = System.IO.File.ReadAllBytes(ofd.FileName); // 선택한 파일을 바이트 배열로 읽어옴
 
-                L1 = bytes.Length;
-                L2 = bytes.Length;
+                L1 = bytes.Length; // 원본 파일 크기
+                L2 = bytes.Length; // 압축 후 파일 크기
 
                 SaveFileDialog sfd = new SaveFileDialog();
-                sfd.Filter = "Zip File|*.zip";
+                sfd.Filter = "Zip File|*.zip"; // 파일 형식 .zip
 
+                // 사용자가 파일을 저장할 위치
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
-                    System.IO.File.WriteAllBytes(sfd.FileName, bytes);
+                    System.IO.File.WriteAllBytes(sfd.FileName, bytes); // 선택한 위치에 파일을 저장
                     report.Text = string.Format("Report: Input File is {0} Bytes and Compressed file is {1} bytes", L1, L2);
-
                 }
-
             }
         }
 
+        /// <summary>
+        /// Decompress 버튼 클릭 시 실행
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Decompress_Click(object sender, EventArgs e)
         {
             OpenFileDialog opf = new OpenFileDialog();
-            opf.Filter = "Zip File|*.zip";
+            opf.Filter = "Zip File|*.zip"; // 파일 형식 .zip
 
+            // 사용자가 파일을 선택
             if (opf.ShowDialog() == DialogResult.OK)
             {
                 int L1, L2;
-                var bytes = System.IO.File.ReadAllBytes(opf.FileName);
+                var bytes = System.IO.File.ReadAllBytes(opf.FileName); // 선택한 파일을 바이트 배열로 읽어옴
 
-                L1 = bytes.Length;
-                L2 = bytes.Length;
+                L1 = bytes.Length; // 원본 파일 크기
+                L2 = bytes.Length; // 해제 후 파일 크기 (현재는 압축 해제 기능이 없어서 동일)
 
                 SaveFileDialog sfd = new SaveFileDialog();
+                // 사용자가 파일을 저장할 위치
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
-                    System.IO.File.WriteAllBytes(sfd.FileName, bytes);
+                    System.IO.File.WriteAllBytes(sfd.FileName, bytes); // 선택한 위치에 파일을 저장
                     report.Text = string.Format("Report: Input File is {0} Bytes and Decompressed file is {1} bytes", L1, L2);
                 }
             }
         }
-
     }
 }
--- Utilities.cs
+++ Utilities.cs
@@ -7,32 +7,43 @@ namespace Utilities
 {
     public class Compressor
     {
+        /// <summary>
+        /// 바이트 배열 압축
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
         public static byte[] CompressBytes(byte[] bytes)
         {
             using (var outputStream = new MemoryStream())
             {
+                // GZipStream으로 사용해 스트림으로 압축
                 using (var compressionStream = new GZipStream(outputStream, CompressionLevel.Optimal))
                 {
                     compressionStream.Write(bytes, 0, bytes.Length);
                 }
-                return outputStream.ToArray();
+                return outputStream.ToArray(); // 압축된 바이트 배열 반환
             }
         }
 
+        /// <summary>
+        /// 압축된 바이트 배열 해제
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
         public static byte[] DecompressBytes(byte[] bytes)
         {
             using (var inputStream = new MemoryStream(bytes))
             {
                 using (var outputStream = new MemoryStream())
                 {
+                    // GZipStream으로 압축된 스트림 압축 해제
                     using (var compressionStream = new GZipStream(inputStream, CompressionMode.Decompress))
                     {
                         compressionStream.CopyTo(outputStream);
                     }
-                    return outputStream.ToArray();
+                    return outputStream.ToArray(); // 압축 해제된 바이트 배열 반환
                 }
             }
         }
-
     }
 }
